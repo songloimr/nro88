8b3ad9143abba6d9bdd6e17883a3632e2ac1a5d0
songloimr <47079927+songloimr@users.noreply.github.com>
2021-11-29 03:53:37 +0000

    Deploying to gh-pages from @ songloimr/nro88@32c582417c9b01baa344237e316a1b1d0f012cb7 🚀

---
+++ precache-manifest.35e0e57b2a4352deed43ff6d9f1812ca.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1f79cf4fdbf5fa189a2",
+    "url": "/admin-one-vue-bulma-dashboard/css/app.fcd51fed.css"
+  },
+  {
+    "revision": "0e480abdb7a35c2e9b8571d68187b4d7",
+    "url": "/admin-one-vue-bulma-dashboard/data-sources/clients.json"
+  },
+  {
+    "revision": "9ed33b873e49a44feb61a3e2fea0dc2a",
+    "url": "/admin-one-vue-bulma-dashboard/favicon.png"
+  },
+  {
+    "revision": "7298fabbecbc6f87cf690f717af8652c",
+    "url": "/admin-one-vue-bulma-dashboard/img/justboil-logo.7298fabb.svg"
+  },
+  {
+    "revision": "2cd529b1b3127330225bd08c08bc0610",
+    "url": "/admin-one-vue-bulma-dashboard/index.html"
+  },
+  {
+    "revision": "f1f79cf4fdbf5fa189a2",
+    "url": "/admin-one-vue-bulma-dashboard/js/app.55543936.js"
+  },
+  {
+    "revision": "d7f88ddf26bedf5eeb8b",
+    "url": "/admin-one-vue-bulma-dashboard/js/chunk-vendors.0baa7d12.js"
+  },
+  {
+    "revision": "100184f42e8c7845b6b7",
+    "url": "/admin-one-vue-bulma-dashboard/js/client-form.8a22a57e.js"
+  },
+  {
+    "revision": "f167454e61ccd8e2982b",
+    "url": "/admin-one-vue-bulma-dashboard/js/client-form~forms.ee5842a5.js"
+  },
+  {
+    "revision": "49f2e75c38b644481802",
+    "url": "/admin-one-vue-bulma-dashboard/js/forms.4335ac1a.js"
+  },
+  {
+    "revision": "9e5a21cf1632b99f5484",
+    "url": "/admin-one-vue-bulma-dashboard/js/profile.b7642a33.js"
+  },
+  {
+    "revision": "d889845cc9efb4f69926",
+    "url": "/admin-one-vue-bulma-dashboard/js/tables.d06d1702.js"
+  },
+  {
+    "revision": "9e02268cb14385af9653507bc8b8ee72",
+    "url": "/admin-one-vue-bulma-dashboard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/admin-one-vue-bulma-dashboard/robots.txt"
+  }
+]);
